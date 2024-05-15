7fc8f1b29ce446c70bdfb91ee40e744a128c5554
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-05-15 04:30:32 -0400

    Fix spelling mistake

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
       <div className="container-fluid">
           <div className='row'>
               <div className='col-6'>
-                  <p className='fs-5 ps-3 fw-bold'>Vasding skill scaning!</p>
+                  <p className='fs-5 ps-3 fw-bold'>Vasding skill scanning!</p>
               </div>
               <div className='col-6 pt-2'>
                   <p className='float-end pe-3 cursor-pointer'>Login | Register</p>
