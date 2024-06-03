b81b74a0197045b91e5fe57223709f50c5b61a8b
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-06-03 00:07:23 -0400

    Change brand name

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,9 @@ function App() {
   return (
     <div>
       <div className="container-fluid">
-          <div className='row'>
-              <div className='col-6'>
-                  <p className='fs-5 ps-3 fw-bold'>Vasding skill scanning!</p>
-              </div>
-              <div className='col-6 pt-2'>
-                  {/* <p className='float-end pe-3 cursor-pointer'>Login | Register</p> */}
-              </div>
-          </div>
-          <Scan/>
-        </div>
+        <p className='fs-5 ps-3 fw-bold'>skill aligner | scanning</p>
+        <Scan />
+      </div>
     </div>
   );
 }
