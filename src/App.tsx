fe40015df51dff5877cbcdbadd6409f9d3b0be47
arifuzzaman.tanin <arifuzzaman.tanin@petronas.com>
2024-12-04 23:59:15 -0500

    Add profile link

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,24 @@ function App() {
   return (
     <div>
       <div className="container-fluid">
-        <p className='fs-5 ps-3 fw-bold'>skill aligner | scanning</p>
+        <div className='row'>
+          <div className='col-lg-6 col-sm-12'>
+            <p className='fs-5 ps-3 fw-bold'>skill aligner | scanning</p>
+          </div>
+
+          <div className='col-lg-6 col-sm-12'>
+            <a
+              href='https://arifuzzaman-tanin.github.io/personal-profile/'
+              target='_blank'
+              rel='noopener'
+              className='ps-3 pe-3 text-primary mt-1 text-sm-start text-lg-end text-decoration-none'
+            >
+              Developed By Arifuzzaman Tanin
+            </a>
+          </div>
+
+        </div>
+
         <Scan />
       </div>
     </div>
